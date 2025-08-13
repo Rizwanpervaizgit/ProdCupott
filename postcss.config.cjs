872614a10013bea8e67c3e5f57afe2f329f9a505
rizwan <rizwanpervaizwork@gmail.com>
2025-08-13 15:25:21 +0500

    UPdated

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss')({
-      config: './tailwind.config.cjs', // or .js if you didn't rename it
-    }),
-    require('autoprefixer'),
-  ],
-}
